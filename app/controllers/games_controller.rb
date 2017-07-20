9ea7ba4a06835ddaaa59aaa7de710fc97c5d5602
Ray Mulligan <rmullig2@optonline.net>
2017-07-19 20:07:18 -0400

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,25 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
-
+  def create
+    #binding.pry
+    @game = Game.create(state: params[:state])
+    redirect_to game_path(@game)
+  end
+  
+  def show
+    @game = Game.find(params[:id])
+    render json: @game
+  end
+  
+  def update
+    @game = Game.find(params[:id])
+    @game.update(state: params[:state])
+    redirect_to game_path(@game)
+  end
+  
+  def index
+    @games = Game.all
+    render json: @games
+  end
+  
 end
