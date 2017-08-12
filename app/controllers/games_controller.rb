2658315bd26349093c5d8e0469c638b26135c86d
Mary <mumoh@mozilla.com>
2017-08-12 16:50:31 -0400

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,13 @@
 class GamesController < ApplicationController
-  # Add your GamesController code here
 
+def create
+    @game = Game.create(games_params)
+    render json: game, status: 201
+end
+
+private
+
+def games_params
+    params.permit(:state)
+end
 end
