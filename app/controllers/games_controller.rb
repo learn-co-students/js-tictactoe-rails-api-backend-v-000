9f5fc4cec679121e66deb901bbfe69b6fa5d75d4
machen2 <victoriamachen@gmail.com>
2017-10-20 22:43:41 -0500

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,28 @@
 class GamesController < ApplicationController
-  # Add your GamesController code here
+    def index
+      @games = Game.all 
 
+    end 
+
+    def show
+      @game = Game.find(params[:id])
+
+    end
+
+    def create
+      @game = Game.create(game_params)
+
+    end
+
+    def update 
+      @game = Game.find(params[:id])
+      @game.update(game_params)
+
+    end 
+
+    private 
+
+    def game_params
+      params.permit(state: [])
+    end
 end
