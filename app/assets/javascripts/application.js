2f4034818e57d19b975dba3c9a42a615014f7edc
Michael Burak <michaelrburak@gmail.com>
2017-11-19 14:20:16 -0500

    Done.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,4 +11,5 @@
 // about supported directives.
 //
 //= require jquery
+//= require home
 //= require jquery_ujs
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @game = Game.new
   end
 end
