a65520ed1f9fc8812fbff5b2f6b295b3b6f3ca88
Sophia Hubeny <shhubeny@gmail.com>
2017-12-06 16:52:43 +0000

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,21 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def index
+    render :'home/index'
+  end
+
+  def show
+    render :'home/index'
+  end
+
+  def create
+
+    render :'home/index'
+  end
+
+  def update
+
+    render :'home/index'
+  end
 
 end
