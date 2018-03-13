eed7bffe63555e60d3494c269bc6305a954a6bf9
Hannah Carnes <hecarnes@gmail.com>
2018-03-12 20:03:04 -0400

    done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,31 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def index
+    @games = Game.all
+    render json: @games, each_serializer: GameSerializer
+  end
 
+  def update
+    @game = Game.find(params[:id])
+    if @game.update(game_params)
+      render json: @game
+    end
+  end
+  
+  def create
+    @game = Game.create(game_params)
+    @game.save
+    render json: @game, status: 201
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    render json: @game
+  end
+
+  private
+
+  def game_params
+    {state: params.require(:state)}
+  end
 end
