33f3e83931ee2131e348c0aa9185104a662bd1fa
github username <schun08@gmail.com>
2018-03-20 21:13:11 -0400

    Done.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,5 @@
 class Game < ActiveRecord::Base
+  # fixes ActiveRecord issue so that state is stored as an Array in a TEXT-typed database column
+  # use .serialize method provided by ActiveRecord::Base
+  serialize :state, Array
 end
