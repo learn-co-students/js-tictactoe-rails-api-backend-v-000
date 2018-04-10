fa0143d874d8744c5de6c52b6bb9f04aae5e741b
chrisjamesr <chrisjames.r@gmail.com>
2018-04-09 21:52:26 -0400

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,34 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  
+  def index
+    @games = Game.all
+    render :json => @games, :status  => 200
+  end
+
+  def create
+    @game = Game.new(state: params[:state])
+    if @game.save
+      render :json => @game, :status => 201
+    end
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    render :json => @game, :status => 200
+  end
+
+  def update
+    # binding.pry
+    @game = Game.find(params[:id])
+    @game.update(state: params[:state])
+    render :json => @game, :status => 202
+  end
+
+  private
+
+    def game_params
+      params.require(:game).permit(:state)
+    end
 
 end
