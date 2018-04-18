406b10c9f28566df29e46500c1d9d1c3bc464eb4
EC2 Default User <ec2-user@ip-172-31-46-48.us-east-2.compute.internal>
2018-04-18 21:37:17 +0000

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,22 @@
 class GamesController < ApplicationController
-  # Add your GamesController code here
-
+  def index
+    @games = Game.all
+    render json: @games
+  end
+  
+  def create
+    @game = Game.create(state: params[:state])
+    render json: @game, status: :created
+  end
+  
+  def show
+    @game = Game.find(params[:id])
+    render json: @game, status: :found
+  end
+  
+  def update
+    @game = Game.find(params[:id])
+    @game.update(state: params[:state])
+    render json: @game
+  end
 end
