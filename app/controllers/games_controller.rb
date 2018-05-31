3c7070d5911b0ae2b8b9957e6ca691ca8cb2ece6
jacobsilver2 <jacobsilver2@mac.com>
2018-05-31 10:13:14 -0400

    working on update

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,22 +1,35 @@
 class GamesController < ApplicationController
-  # Add your GamesController code here
+  
+  before_action :set_game, only: [:show, :update]
+
+  
   def create
-    Game.create(game_params)
-    
+    @game = Game.create(game_params)
+    render json: @game, status: 201
   end
 
   def show
+    render json: @game, status: 201
   end
 
   def update
   end
 
   def index
+    @games = Game.all
+  end
+
+  def new
+    @game = Game.new
   end
 
   private
   def game_params
-    params.require(:game).permit(:state)
+    params.permit(state: [])
+  end
+
+  def set_game
+    @game = Game.find(params[:id])
   end
 
 end
