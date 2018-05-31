0041573bb708b22325d9f0eeb06a22029672dffe
jacobsilver2 <jacobsilver2@mac.com>
2018-05-31 09:56:20 -0400

    first commit

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,22 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def create
+    Game.create(game_params)
+    
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  private
+  def game_params
+    params.require(:game).permit(:state)
+  end
 
 end
