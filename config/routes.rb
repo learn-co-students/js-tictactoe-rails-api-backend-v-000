4a029a8ea6c5092b6fd69464e4e1e44a1f6661c8
github username <github email address>
2018-06-01 09:51:53 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
-  # Example of regular route:
+  resources :games, only: [:show, :index, :update, :create] # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
