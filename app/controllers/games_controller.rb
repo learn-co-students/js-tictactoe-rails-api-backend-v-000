e39c6738d1d3daf50c134740a665b831ffa16c01
Christina <github email address>
2018-06-01 15:05:59 -0600

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,36 @@
 class GamesController < ApplicationController
+  before_action :find_game, only: [:show, :update]
   # Add your GamesController code here
+  # json serializers and have four endpoints
+  # create, show, index, update
+
+  def create #POST /games
+    game = Game.create(game_params)
+    render json: game
+  end
+
+  def show #GET /games/:id
+    render json: @game
+  end
+
+  def index #GET /games
+    games = Game.all
+    render json: games
+  end
+
+  def update #PATCH /games/:id
+    @game.update(game_params)
+    render json: @game
+  end
+
+  private
+
+  def game_params
+    params.permit(state: [])
+  end
+
+  def find_game
+    @game = Game.find(params[:id])
+  end
 
 end
