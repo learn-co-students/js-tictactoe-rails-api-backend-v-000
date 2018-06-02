8489c59b8bdd097022dfc11a7f7b75c2a21e37a6
Lizzie <lizzie.sorin@hotmail.fr>
2018-06-02 12:59:22 -0700

    set CRUD

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,28 @@
 class GamesController < ApplicationController
-  # Add your GamesController code here
+before_action :set_game, only: [:show, :update]
+
+	def index 
+		@games = Game.all
+	end
+
+	def create
+
+	end
+
+	def show
+		respond_to do |format|
+			format.html
+			format.json {render json: @game}
+		end
+	end
+
+	def update
+	end
+
+private
+
+	def set_game
+		@game = Game.find(params[:id])
+	end
 
 end
