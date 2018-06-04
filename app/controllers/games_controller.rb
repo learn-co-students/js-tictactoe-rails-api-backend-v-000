681476d6eb702e2a8be89b6c7b70af820c2b4778
terryblue99 <terryblue@comcast.net>
2018-06-04 13:51:18 -0500

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,46 @@
 class GamesController < ApplicationController
-  # Add your GamesController code here
+  before_action :set_game, only: [:show, :edit, :update]
+
+  def index
+    render plain: Game.all
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+  # 	raise params.inspect
+    @game = Game.create(game_params)
+    render json: @game, status: 201
+  end
+
+  def edit
+  end
+
+  def update
+    @game.update(post_params)
+    binding.pry
+    redirect_to post_path(@game)
+  end
+
+  def show
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @game, status: 200 }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_game
+    @game = Game.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def game_params
+    params.fetch(:game, {}).permit(:state)
+  end
 
 end
