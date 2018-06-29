c67497a668a6d1a90a35ef617409f3d2369c26b0
Olena Ageyeva <eageeva@hotmail.com>
2018-06-29 19:52:15 +0000

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,14 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def initialize()
+    @state=[]
+  end
 
+  def create()
+    Game.create()
+    render '/home/index'
+  end
+
+  def index
+  end
 end
