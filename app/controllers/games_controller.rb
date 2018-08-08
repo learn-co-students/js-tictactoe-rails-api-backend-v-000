88eb139ea9112bcad73cb294ff03b73d6043b171
github username <github email address>
2018-08-07 22:52:59 -0700

    starting on GamesController #create

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,15 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+def new
+end
 
+def create
+  @game = Game.create(params["state"])
+  render json: @game
+end
+
+private
+  def game_params
+    params.require(:games).permit(:id, :state)
+  end
 end
