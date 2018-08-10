65fc3d7781354130c8ee5675caf4c2fd975f6d1e
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-10 00:53:16 +0000

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,17 @@
 class GamesController < ApplicationController
-  # Add your GamesController code here
+
+  def create
+    @game = Game.create(state: params[:state])
+    render json: @game, status: 201
+  end
+
+  def show
+  end 
+
+  def update
+  end
+
+  def index
+  end
 
 end
