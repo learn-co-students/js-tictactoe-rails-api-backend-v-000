6b450fc8089397910e59c5568809fe4e12704b91
nmlstyl <joey.michalina.mariano@gmail.com>
2018-08-19 19:24:09 -0400

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,19 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
 
 end
