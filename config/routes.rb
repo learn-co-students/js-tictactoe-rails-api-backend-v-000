d3fa8fb71ce04b8885ee7de04df805abdd7baee9
Maverick Tesini <mavericktesini@gmail.com>
2018-08-23 04:20:16 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  # get '/games/index', to: 'games#index'
+  # post '/games', to: 'games#create'
+  # get '/games/:id', to: 'games#show'
+  # patch '/games/:id', to: 'games#update'
+
+  resources :games, only: [:create, :update, :show, :index]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
