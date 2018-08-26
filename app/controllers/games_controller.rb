959d75b814a4d43dfc8488598ad9e6123e4ade36
dwieb <dwayne.harmon@gmail.com>
2018-08-26 17:40:27 -0400

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,24 @@
 class GamesController < ApplicationController
-# Create — POST — /games
-# Show — GET — /games/:id
-# Update — PATCH — /games/:id
-# Index — GET — /games
+
+  def index
+    @games = Game.all
+    render json: @games
+  end
+
+  def create
+    Game.create(state: ["X", "", "", "", "", "", "", "", ""])
+    render :'home/index'
+  end
+
+  def show
+    @game = Game.find_by(id:params[:id])
+    render json: @game
+  end
+
+  def update
+    @game = Game.find_by(id: params[:id])
+    @game.update(state: params[:state])
+    render :'home/index'
+  end
 
 end
