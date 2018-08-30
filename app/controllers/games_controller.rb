5622ac12c50ac34e64574aabd890a521b2f63fce
learn <eric.desimone@cortland.edu>
2018-08-30 12:54:41 -0700

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,8 @@
 class GamesController < ApplicationController
 
   def index
+    @games = Game.all
+    render json: @games
   end
 
   def create
@@ -9,9 +11,14 @@ class GamesController < ApplicationController
   end
 
   def update
+    @game = Game.find(params[:id])
+    @game.update(state: params[:state])
+    render json: @game
   end
 
   def show
+    @game = Game.find(params[:id])
+    render json: @game
   end
 
 private
