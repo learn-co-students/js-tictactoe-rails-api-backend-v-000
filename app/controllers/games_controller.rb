8e2054b9daad534c93a2a2cb846b6428545a0d51
ktamoguis <ktamoguis@gmail.com>
2018-09-05 05:47:23 +0800

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,11 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def create
+    binding.pry
+    @state = JSON.stringify(params[:state])
+    binding.pry
+    @game = Game.create(state: params[:state])
+    render plain: @game.state
+  end
 
 end
