1cf310a8738f0a73dabdb25a609e36df3e8b2a7d
ktamoguis <ktamoguis@gmail.com>
2018-09-05 09:37:24 +0800

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,10 +2,18 @@ class GamesController < ApplicationController
   # Add your GamesController code here
   def create
     binding.pry
-    @state = JSON.stringify(params[:state])
+    #@state = JSON.stringify(params[:state])
     binding.pry
     @game = Game.create(state: params[:state])
+    #@game.state = @game.state.gsub( "\"", "")
+    @game.save
+    binding.pry
     render plain: @game.state
+    binding.pry
+    #respond_to do |format|
+    #  format.html { render :show }
+    #  format.json { render json: @game}
+    #end
   end
 
 end
