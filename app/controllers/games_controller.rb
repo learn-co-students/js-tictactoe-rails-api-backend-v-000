7c2cc895821adc1d063d1b251fa3a0e0aa72b0e8
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-09-24 11:58:37 -0700

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,4 +7,19 @@ class GamesController < ApplicationController
     render 'home/index'
   end
 
+  def show
+    @game = Game.find(params[:id])
+    render json: @game
+  end
+
+  def update
+    @game = Game.find(params[:id])
+    @game.update(:state => params[:state])
+    render 'home/index'
+  end
+
+  def index
+    render json: Game.all
+  end
+
 end
