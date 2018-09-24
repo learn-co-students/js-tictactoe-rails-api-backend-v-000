9f53d4393c9201179d700f3b4d284247e09e0c59
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-09-23 18:24:47 -0700

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,10 @@
+require 'pry'
 class GamesController < ApplicationController
   # Add your GamesController code here
+
+  def create
+    @game = Game.create(:state => params[:state])
+    render 'home/index'
+  end
 
 end
