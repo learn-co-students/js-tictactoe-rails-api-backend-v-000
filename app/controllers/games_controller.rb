c25c6eff259ea7556b847f625c8aae9c0ee016d0
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-10-04 16:22:34 -0400

    Implement games#show

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,16 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
   def create
-    @game = Game.create({state: params[:state]})
-    render json: @game
+    game = Game.create({state: params[:state]})
+    render json: game
+  end
+
+  def show
+    game = Game.find_by(id: params[:id])
+    if game
+      render json: game
+    else
+      redirect_to :index
+    end
   end
 end
---
+++ config/initializers/active_model_serializers.rb
@@ -0,0 +1,3 @@
+ActiveModelSerializers.config.adapter = :json_api
+ActiveModelSerializers.config.jsonapi_include_toplevel_object = true
+ActiveModelSerializers.config.jsonapi_version = '1.0'
