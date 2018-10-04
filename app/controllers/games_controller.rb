c7ce64bf9595d712a993ce86a5e2e43dec626257
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-10-04 16:13:01 -0400

    Implement games#create

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,7 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
-
+  def create
+    @game = Game.create({state: params[:state]})
+    render json: @game
+  end
 end
