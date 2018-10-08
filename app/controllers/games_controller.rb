2014d23d03f88da990002ba1071f6e5bb72fb27f
Suchitra Naik <susanaik@gmail.com>
2018-10-08 20:57:18 +0000

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,29 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
 
+  def index
+  end
+
+  def new
+    @game = Game.new
+   end
+
+  def create
+    @game = Game.create(game_params)
+    redirect_to games_path
+  end
+
+  def show
+
+  end
+
+  def update
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:state)
+  end
+
 end
