9cee14101c7407cffc4aff9c645662df3fbcf5c3
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-10-12 15:31:27 -0400

    controller actions

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,22 +1,36 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
   def new
+    @game = Game.new
   end
 
   def create
+    @game = Game.create(game_params)
+    render json: @product, status: 201
   end
 
   def index
+    @games = Game.all
   end
 
   def show
+    @game = Game.find(params[:id])
+    respond_to do |format|
+     format.html { render :show }
+     format.json { render json: @game }
+   end
   end
 
   def edit
   end
-  
+
   def update
   end
 
+  private
+
+  def game_params
+     params.require(:game).permit(:state, :created_at, :updated_at)
+  end
 
 end
