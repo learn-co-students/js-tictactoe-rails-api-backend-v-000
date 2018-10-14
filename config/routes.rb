ca6c19e86917c5a5c2319a22a6a605f910c4ef99
github username <github email address>
2018-10-13 22:55:55 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
-  resource :game, only [:index, :show, :crate, :update, :destroy]
+  resources :games, only: [:index, :show, :create, :update, :destroy]
 
 end
