8983f78f21757292568e1e004d8cee9b8fc8e78d
github username <github email address>
2019-01-22 19:46:35 -0500

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,31 @@
+require 'pry'
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def create
+    #binding.pry
+    @game = Game.create(state: params[:state]).to_json
+    redirect_to games_path
+    #binding.pry
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    render json: @game
+
+  end
+
+  def update
+    #binding.pry
+    @game = Game.find(params[:id])
+    @game.update(state: params[:state])
+    redirect_to games_path
+  end
+
+  def index
+    @games = Game.all
+
+    render json: @games
+
+  end
 
 end
