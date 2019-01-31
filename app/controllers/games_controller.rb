1e62c9441fdc42aad9c54f422e2c1cc6a8b9712c
Vibraniumforge <gregm9999@gmail.com>
2019-01-31 15:20:13 -0800

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,34 @@
 class GamesController < ApplicationController
-  # Add your GamesController code here
+  
+  before_action :game_finder, only: [:show, :update]
+
+  def create
+    @game = Game.create(game_params)
+    render json: @game
+  end
+
+  def show
+    render json: @game
+  end
+
+  def update
+    @game.update(game_params)
+    render json: @game
+  end
+
+  def index
+    @games=Game.all
+    render json: @games
+  end
+
+  private
+
+    def game_params
+      params.permit(:id, state:[])
+    end
+
+    def game_finder
+      @game=Game.find(params[:id])
+    end
 
 end
