cb6ae6a420660fb5b72873cd4eb53b0619e25ebe
freeWillee <will.ik.lee@gmail.com>
2019-03-11 21:50:05 -0600

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,9 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def create
+    game = Game.create()
+    game.state = []
+    render json: game.to_json()
+  end
 
 end
