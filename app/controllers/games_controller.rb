9540c292be399cb65d6a5d5ad7d1d243863afbc2
freeWillee <will.ik.lee@gmail.com>
2019-03-11 21:59:50 -0600

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,14 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
   def create
-    game = Game.create()
-    game.state = []
-    render json: game.to_json()
+    game = Game.create(game_params)
+    render json: game, status: 201
+  end
+
+  private
+
+  def game_params
+    params.permit(state: [])
   end
 
 end
