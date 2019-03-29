68fe81bcf29e5e501ad0d52f3849df2c63e087a4
github username <github email address>
2019-03-29 13:17:41 -0400

    Create a new Game instance and serialize the state

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,13 @@
 class GamesController < ApplicationController
-  # Add your GamesController code here
-
+    # Add your GamesController code here
+    def games
+    end
+    def create
+        game = Game.create(state: params[:state])
+        render json: game, status: 201
+    end
+    def show
+    end
+    def update
+    end 
 end
