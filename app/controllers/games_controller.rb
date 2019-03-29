0d356a0c70608ddca045fd4f95de1a68dd287c55
github username <github email address>
2019-03-29 14:04:43 -0400

    Done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,27 @@
 class GamesController < ApplicationController
+    before_action :set_game, only: [:show, :update]
     # Add your GamesController code here
-    def games
+    def index
+        @games = Game.all
+        render json: @games, status:200
     end
     def create
-        game = Game.create(state: params[:state])
+        game = Game.create(game_params)
         render json: game, status: 201
     end
     def show
+        render json: @game, status:200
     end
-    def update
+    def update        
+        @game.update(state: params[:state])
+        render json: @game, status:200
     end 
+    private
+    def set_game
+        @game = Game.find(params[:id])
+    end
+  
+    def game_params
+        params.permit(state:[]) 
+    end
 end
