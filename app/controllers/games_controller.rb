673e6f4ef2a84313214c239be03f71e43aa8ebf6
Mika88 <mikaspivak@gmail.com>
2019-04-19 14:52:14 +0000

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,26 @@
 class GamesController < ApplicationController
-  # Add your GamesController code here
+  def index
+    @games = Game.all
+    render json: @games
+  end
 
+  def show
+    render json: get_game, status: 200
+  end
+
+  def create
+     @game = Game.create(state: params[:state])
+     render json: @game, status: 201
+  end
+
+  def update
+    @game = get_game.update(state: params[:state])
+    render json: @game, status: 201
+  end
+
+  private
+
+  def get_game
+    Game.find(params[:id])
+  end
 end
