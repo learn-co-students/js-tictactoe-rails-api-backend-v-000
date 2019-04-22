2987f7c081c6434e20cb0ed60b348f3205cf4e87
Christia Tidwell <christiatidwell@Christias-MBP.attlocal.net>
2019-04-22 13:52:29 -0500

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,27 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def index
+    @games = Game.all
+    render json: @games
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    render json: @game
+  end
+
+  def create
+    @game = Game.create(state: params[:state])
+    render json: @game
+  end
+
+  def update
+    @game = Game.find(params[:id])
+    @game.update(state: params[:state])
+
+    @game.save
+
+    render json: @game
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+  resources :games, only: [:show, :index, :udpate, :create]
+  get '/games/:id/:state', to: 'games#update'
 end
