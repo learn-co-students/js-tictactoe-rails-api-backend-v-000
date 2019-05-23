b32e99005379c470cb41eabb4512f4c0955e05cf
Rachel Wyatt <rw126@evansville.edu>
2019-05-23 07:32:15 -0400

    add create and game_params methods to games_controller.rb

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,18 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def create 
+    game = Game.create(game_params)
+    render json: game 
+  end
+
   def index #GET /games
     games = Game.all 
     render json:games 
+  end
+
+  private 
+
+  def game_params 
+    params.permit(state: [])
   end
 end
