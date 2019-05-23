2962382f83a7420b21ae2a9b13289676e488c6df
harleyharl <harleymteden@gmail.com>
2019-05-23 10:21:24 -0700

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,27 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def index
+  end
 
+  def show
+    @game = Game.find(params[:id])
+    render json: @game, status: 200
+  end
+
+  def new
+  end
+
+  def create
+    #uses the serialize method in the Game model
+    Game.create(state: params[:state])
+  end
+
+  def update
+  end
+
+  private
+
+  def game_params
+    params.require(:state).permit(:state)
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
 end
