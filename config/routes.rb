592ac07d37293cebed9367de35fe7e6d1da1e7a1
kjdub01 <kjewettwilliams@gmail.com>
2019-06-08 17:01:45 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+  resources :games, only: [:create, :show, :index, :update]
+  #get 'games/:id/game_data', to: 'games#game_data'
 end
