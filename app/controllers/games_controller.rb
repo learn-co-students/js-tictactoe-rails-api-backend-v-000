03143dbd42abeef771831acca0de203feac35cdf
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-06-26 16:57:24 -0400

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,34 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
 
+  # def new
+  #   @game = Game.new
+  # end
+
+  def index
+    render json: Game.all
+  end
+  
+  def create
+    @game = Game.create(game_params)
+    #render json: @game.to_json(only: [:state, :id])
+    render json: @game, status: 201
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    render json: @game
+  end
+
+  def update
+    Game.find(params[:id]).update(game_params)
+    render json: @game
+  end
+
+  private
+  def game_params
+    #As per lab's raised issue:
+    params.permit(:state => [])
+  end
+
 end
