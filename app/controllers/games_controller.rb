a405af5cbc0a6001758c915aca4ad9aad0e41b5a
sarawall1187 <sarawall1187@gmail.com>
2019-07-01 08:14:34 -0700

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,33 @@
 class GamesController < ApplicationController
-  # Add your GamesController code here
 
+  # Create — POST — /games
+  # Show — GET — /games/:id
+  # Update — PATCH — /games/:id
+  # Index — GET — /games
+  def index
+    @games = Game.all
+  end
+
+  def new
+    set_game
+  end
+
+  def create
+    @game = Game.create(game_params)
+    render json: @game, status: 201
+  end
+
+  def show
+    set_game
+  end
+
+  private
+  
+  def set_game
+    @game = Game.find(params[:id])
+  end
+
+  def game_params
+    params.require(:game).permit(:state)
+  end
 end
