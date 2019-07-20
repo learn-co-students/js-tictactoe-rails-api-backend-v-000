726e01c6e8a50097be3133eaddb1a04523ee9928
Naomi-Dennis <njdennis94@gmail.com>
2019-07-20 17:29:58 -0400

    Save Lab

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,24 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  ## POST /games 
+  ## Expected Params: {"game": "[]" }
+  def index
+	all_games_list = Game.all 
+	render json: all_games_list 
+  end 
+  def create
+	  Game.create!(state: params[:state].to_json) 
+  end
+
+  def show
+ 	game_obj = Game.find(params[:id]) 
+	render json: game_obj
+  end
+
+  def update
+	Game.update(id: params[:id], state: params[:state].to_json) 
+  end 
+
+  
 
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,8 @@
 class Game < ActiveRecord::Base
+
+   def state
+	   id = self.id
+	   state = ActiveRecord::Base.connection.execute("SELECT state FROM games WHERE id=#{id}")
+	   JSON.parse state[0]["state"] 
+   end 	
 end
