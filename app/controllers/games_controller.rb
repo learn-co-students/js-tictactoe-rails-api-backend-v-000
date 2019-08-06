1ca450238ed4532d3c0aef5bb008bd3d5da5d2ca
github username <github email address>
2019-08-05 20:35:29 -0400

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,28 +4,31 @@ class GamesController < ApplicationController
   def create #5
     #binding.pry
     @game = Game.create(game_params)
-    @game.save
+  #  @game.save
     render json: @game, status: 201
   end
 
   def show
     @game = Game.find(params[:id])
-    respond_to do |format|
-      format.html { render :show }
-      format.json { render json: @game.to_json(only: [:id, :state])}
-    end
+    render json: @game, status: 201
   end
 
   def update
+    @game = Game.find(params[:id])
+    @game.update(game_params)
+    render json: @game, status: 202
   end
 
   def index
+    @games = Game.all
+    render json: @games, status: 200
   end
+
 
   private
 
   def game_params
-    params.require(:game).permit(:state)
+    params.permit(state: [])
   end
 
 end
--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,6 +1,3 @@
-# class GameSerializer < ActiveModel::Serializer
-#   attributes :id, :state
-# end
-class Game < ActiveRecord::Base
-  serialize :state, Array
+class GameSerializer < ActiveModel::Serializer
+  attributes :id, :state
 end
