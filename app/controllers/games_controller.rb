51aaf71eb397850fc236ab3393b224e4851081c1
sjwakean <swakeman@ca.rr.com>
2019-08-19 11:36:48 -0700

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,42 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  
+  def index
+    @games = Game.all
+    render json: @games, status: 201 #render json instead of html implicity
+  end
+  
+  def new
+    @game = Game.new
+  end
+  
+  def create
+    @game = Game.create(game_params)
+    #Original
+    #redirect_to games_path
+    #Changed to:
+    render json: @game, status: 201
+  end
+  
+  def show
+    @game = Game.find(params[:id])
+      #respond_to do |format|
+        #format.html { render :show }
+        render json: @game, status: 201
+    #end
+  end
 
+  def update
+    @game = Game.find(params[:id])
+    @game.update(game_params)
+    #redirect_to game_path(@game)
+    render json: @game, status: 204
+  end
+
+  private
+  
+  def game_params
+    #params.require(:game).permit(:id, :state)
+    params.permit(state: []) #sets state: an empty array
+  end
 end
